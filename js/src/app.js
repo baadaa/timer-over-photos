ae10d9c8bcbca242c69c8171b6658ec7b665e065
Bumhan Yu <bumhan.yu@gmail.com>
2018-11-05 22:16:12 -0500

    functional photo viewer ready

--- js/src/app.js
+++ js/src/app.js
@@ -2,9 +2,13 @@ import { getTimeString } from './modules/timeStringFormatter';
 import { shuffle } from './modules/shuffle';
 import './modules/flickity.min.css';
 import './modules/style.css';
+import Flickity from 'flickity-bg-lazyload';
+import { imgOptions } from './modules/flkOptions';
+
 
 let isTimer = false;
 
+const photoViewer = document.querySelector('.photo-app');
 
 // DOM Elements
 const progress = document.querySelector('.progress'),
@@ -87,71 +91,59 @@ const timerApp = () => {
     replayBtn.addEventListener('click', resetForReplay);
   }
   init();
-  
 };
-window.onload = function() {
-  timerApp();
-  // const countDown = () => {
-  //   currentSecond -= 1;
-  //   const widthRatio = 1 - (currentSecond / totalSeconds);
-  //   progress.style.width = `${widthRatio * window.innerWidth}px`;
-  //   timer.textContent = currentSecond <= 0 ? "DONE" : getTimeString(currentSecond);
-  //   if (currentSecond <= 0) {
-  //     clearInterval(interval);
-  //     progress.style.opacity = 0;
-  //     toggleReplayBtn();
-  //   } else {
-  //     progress.style.opacity = 1;
-  //   }
-  // }
-  // const toggleReplayBtn = (mode) => {
-  //   if (mode==='reset') {
-  //     replayBtn.classList.add('isHidden');
-  //     playBtn.classList.remove('isHidden');
-  //   } else {
-  //     replayBtn.classList.toggle('isHidden');
-  //     playBtn.classList.toggle('isHidden');
-  //   }
-  // }
-  // const togglePlayBtn = (mode) => {
-  //   if (mode === 'reset') {
-  //     playBtn.querySelector('.playSvg').classList.remove('isHidden');
-  //     playBtn.querySelector('.pauseSvg').classList.add('isHidden');
-  //   } else {
 
-  //     playBtn.querySelector('.playSvg').classList.toggle("isHidden");
-  //     playBtn.querySelector('.pauseSvg').classList.toggle("isHidden");
-  //   }
-  //   if (isPaused) {
-  //     playBtn.querySelector('.txt').textContent = mode === "pause" ? "Resume" : "Start";
-  //   } else {
-  //     playBtn.querySelector('.txt').textContent = "Pause"
-  //   }
-  // }
-  // const toggleTimer = (e) => {
-  //   isPaused = !isPaused;
-  //   if (!isPaused) {
-  //     interval = setInterval(countDown, 1000);
-  //   } else {
-  //     clearInterval(interval);
-  //   }
-  //   togglePlayBtn('pause');
-  // }
-  // const resetTimer = () => {
-  //   if (currentSecond === totalSeconds) return;
-  //   if (currentSecond <= 0) toggleReplayBtn();
-  //   currentSecond = totalSeconds;
-  //   isPaused = true;
-  //   progress.style.width = '0px';
-  //   clearInterval(interval);
-  //   togglePlayBtn('reset');
-  //   timer.textContent = getTimeString(currentSecond);
-  // }
-  // const resetForReplay = () => {
-  //   resetTimer();
-  //   toggleReplayBtn('reset');
-  //   toggleTimer();
-  // }
+const photoViewerApp = () => {
+  const fetchList = () => {
+    fetch(`data/images.json`)
+      .then(res => res.json())
+      .then(resString => {
+        hidePreloader();
+        createFlktyString(shuffle(Array.from(resString)));
+      })
+      .catch(err => console.log(err));
+  };
+
+  const assetString = (filename) => {
+    return `<div class="carousel-cell" data-flickity-bg-lazyload='${filename}'></div>`;
+  }
+  const createFlktyString = (fileArray) => {
+    const viewer = document.querySelector('.photo-app .main-carousel');
+    viewer.innerHTML = fileArray.map(filename => assetString(filename)).join('');
+    console.log(viewer);
+  }
+  const launchFlkty = () => {
+    const viewer = photoViewer;
+    const carousel = viewer.querySelector('.main-carousel');
+    const loadOption = imgOptions;
+    viewer.style.display = "flex";
+    // setTimeout(() => { viewer.style.opacity = 1 }, 0);
+    // setTimeout(() => { toggleFullscreen("on") }, 500);
+    const flkty = new Flickity(carousel, loadOption);
+    flkty.resize();
+  }
+  const init = async () => {
+    // fetchList("images");
+    const imgList = await fetch(`data/images.json`);
+    const json = await imgList.json();
+    await createFlktyString(shuffle(Array.from(json)));
+    await launchFlkty();
+    // resourceType => {
+    //   fetch(`data/${resourceType}.json`)
+    //     .then(res => res.json())
+    //     .then(resString => {
+    //       hidePreloader();
+    //       createFlktyString(shuffle(Array.from(resString)), resourceType);
+    //     })
+    //     .catch(err => console.log(err));
+    // };
+  }
+  init();
+}
+
+window.onload = function() {
+  photoViewerApp();
+  timerApp();
   const toggleApps = () => {
     document.querySelector('.photo-app').classList.toggle('isHidden');
     document.querySelector('.timer-app').classList.toggle('isHidden');
---
+++ js/src/modules/flkOptions.js
@@ -0,0 +1,22 @@
+const imgOptions = {
+  cellAlign: 'center',
+  contain: true,
+  imagesLoaded: true,
+  percentPosition: false,
+  wrapAround: true,
+  autoPlay: 5000,
+  bgLazyLoad: true,
+  bgLazyLoad: 2,
+  pauseAutoPlayOnHover: false
+}
+const vidOptions = {
+  cellAlign: 'center',
+  contain: true,
+  imagesLoaded: true,
+  percentPosition: false,
+  wrapAround: true,
+  autoPlay: false,
+  pauseAutoPlayOnHover: false
+}
+
+export { imgOptions, vidOptions };
